b5d019e9b35c4c794378fa659b8ccf8da343fa18
Tupis <joaoh.tupinamba@gmail.com>
2022-09-13 13:12:24 -0300

    feat: use hooks to fonts + conditional loading to home

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
+import { Background } from './src/components/background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+
+} from '@expo-google-fonts/inter';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ 
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      { fontsLoaded ? <Home /> : <Loading /> }
+    </Background>
   );
 }
 
